87cfb03dc5e90f8ff3bf72394c4bd2eeed47cc53
MrItzreal <is.wsie@gmail.com>
2025-05-31 15:48:43 -0400

    Transferred all of the business logic from 'SchoolController' to the 'SchoolService'

---
+++ src/main/java/com/SchoolService.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import com.codewithizzy.springboot.SchoolDto;
+import com.codewithizzy.springboot.SchoolMapper;
+import com.codewithizzy.springboot.SchoolRepository;
+
+@Service
+public class SchoolService {
+  private final SchoolMapper schoolMapper;
+  private final SchoolRepository schoolRepository;
+
+  // Constructor for repository & studentMapper
+  public SchoolService(SchoolMapper schoolMapper, SchoolRepository schoolRepository) {
+    this.schoolMapper = schoolMapper;
+    this.schoolRepository = schoolRepository;
+  }
+
+  public SchoolDto create(SchoolDto dto) {
+    var school = schoolMapper.toSchool(dto);
+    schoolRepository.save(school);
+    return dto;
+  }
+
+  public List<SchoolDto> findAll() {
+    return schoolRepository.findAll()
+        .stream().map(schoolMapper::toSchoolDto).collect(Collectors.toList());
+  }
+}
